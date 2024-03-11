058a5e5d7ae07f318890a733f382f7e95f488b41
RhumTea <160001872+rhumtea@users.noreply.github.com>
2024-03-10 22:21:13 -0700

    Final Part

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -15,22 +15,15 @@ namespace TodoList
             var appRun = true;
             Console.WriteLine("Welcome to my To Do List");
             // If input start with command, app will do as required.
-            // '-Exit': Exit App
-            // '-Show': Display TodoList
-            // '-Remove': Remove item
-            // default: Add item to TodoList
+            // '3': Exit App
+            // '2': Display TodoList
+            // '1': Add item to TodoList
             while (appRun)
             {
                 Console.WriteLine("What do you want?");
                 Console.WriteLine("1. Add Task");
                 Console.WriteLine("2. Show All Todo List");
-                Console.WriteLine("3. Edit Task List");
-                Console.WriteLine("4. Edit Task");
-                Console.WriteLine("5. Remove Task List");
-                Console.WriteLine("6. Remove Task");
-                Console.WriteLine("7. Filter Task List/ Task");
-                Console.WriteLine("8. Clear Everything");
-                Console.WriteLine("9. Exit");
+                Console.WriteLine("3. Exit");
                 var input = Console.ReadLine();
                 switch (input)
                 {
@@ -44,20 +37,7 @@ namespace TodoList
                     case "2":
                         todo.ShowAllTask();
                         break;
-                    //case "3":
-                    //    Console.WriteLine("Which task do you want to remove?");
-                    //    var taskNameRemove = Console.ReadLine();
-                    //    if (toDoList.Contains(taskNameRemove))
-                    //    {
-                    //        toDoList.Remove(taskNameRemove);
-                    //        Console.WriteLine("Removed {0} \n", taskNameRemove);
-                    //    }
-                    //    else
-                    //    {
-                    //        Console.WriteLine("{0} is not in todo list", taskNameRemove);
-                    //    }
-                    //    break;
-                    case "9":
+                    case "3":
                         appRun = false;
                         break;
                     default:
--- TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b867e96b0efc2136c6d7c78618ca9f2feb5453f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4117568f3e613c8031b432733914052b5c7b809")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
